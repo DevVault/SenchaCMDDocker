f773d54f2521af96b0edc9f98b15ce3ead50e517
Max Vorobyev <gtvolk31@gmail.com>
2021-09-25 00:15:51 +0300

    Configure fix

--- configure.sh
+++ configure.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-_build_images=0
+_build_images=$1
 _image_name="devvault/senchacmd"
 _links=$(curl -L -s https://www.sencha.com/products/extjs/cmd-download | grep -ioP "http:\/\/cdn\.sencha\.com\/cmd\/[\d.]+\/.*amd64\.sh\.zip" | sort -u)
 _versions=$(echo "${_links}" | grep -ioP "\d+\.\d+\.\d+\.{0,1}\d{0,}" | sort -u)
+
+if [[ "${_build_images}" == "1" ]]; then
+	echo "WARNING! Images are begin build and sent to docker hub!"
+fi
 
 while IFS= read -r _version; do
   sed -e "s/\%SENCHA\_VERSION\%/${_version}/g" "$(pwd)/Dockerfile.template" > "$(pwd)/Dockerfile"
